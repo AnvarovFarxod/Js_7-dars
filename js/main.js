db8601b645abb82b947938287cf5f2209d9902d5
aqdtjjksytjksfyksk <134036555+AnvarovFarxod@users.noreply.github.com>
2023-05-20 13:05:23 +0500

    JS 7-dars

---
+++ js/main.js
@@ -0,0 +1,60 @@
+/* Matn uzunligini topish */
+/*let matn = "Assalomu alaykum shogirdlar!";
+console.log(matn.length);*/
+
+/* indexOf (matn ichidan so'zning boshlanish indexini topish) */
+/*let matn = "sen gapir sen men esa sen bilan yozib boramiz";
+let birinchiSenIndex = matn.indexOf('sen');
+console.log(matn.indexOf('sen',birinchiSenIndex+1));*/
+
+/* replace (so'zlarni almashtirib qo'yish) */
+/*
+let matn = "Salom hammaga! salom berdim hammaga! salom"
+/!*console.log(matn.replace(/salom|hammaga|berdim/gi,'alik'));*!/
+let yangiMatn = matn.replace(/salom|hammaga/gi,function (soz){
+  if (soz == 'Salom' || soz == 'salom'){
+      return 'alik';
+  }  else if (soz == 'hammaga'){
+      return 'barchaga';
+  }
+});
+console.log(yangiMatn);*/
+
+/* Push & Pop (Arrayga ma'lumot kiritish va chiqarib olish) */
+/*let list = [];
+list.push('Firemen');
+list.push('Freedom');
+list.push('Police');
+list.push('Oloviddin ayronchi');
+list.push('Saman');
+list.push("Ozod do'xtir");
+console.log(list);
+console.log(list.pop());
+console.log(list);
+console.log(list.pop());
+console.log(list);
+console.log(list.pop());
+console.log(list);*/
+
+/* Unshift & Shift (Arrayga ma'lumot kiritish va chiqarib olish) */
+/*let list = [];
+list.unshift('Olov');
+list.unshift('Komol');
+list.unshift('Bekzod');
+list.unshift(14);
+console.log(list);
+console.log(list.shift());
+console.log(list);
+console.log(list.shift());
+console.log(list);
+console.log(list.shift());
+console.log(list);*/
+
+/* sort (Tartibga keltirish) */
+/*let list = ['Kamal','Saman','OlovIT','Ozod','Bekzod','Muhammadamin','Shaxzod'];
+console.log(list.sort().reverse());*/
+/*let sonlar = [6,89,5,7,44,29,123,11];
+let tartiblanganSonlar = sonlar.sort(function (a,b){
+    return a-b;
+})
+console.log(tartiblanganSonlar.reverse());*/
